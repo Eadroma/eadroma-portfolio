96ff8aaba7034afd39cefd3ddcf9720062d15460
Eadroma <boukottaya.jr@gmail.com>
2022-02-07 23:54:13 +0100

    [ADD] basic setup

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import Navbar from './components/Navbar.vue'
+</script>
+
+<template>
+<div>
+  <Navbar />
+</div>
+</template>
+
+<style>
+@import './scss/main.scss';
+</style>
