83ccf1a65fb096bd55800f48f3a4d97ffabfdede
Eadroma <boukottaya.jr@gmail.com>
2022-02-07 23:54:24 +0100

    [ADD] Navbar component

---
+++ src/components/Navbar.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="Navbar">
+    <span class="leftNavbar">
+      <img src="/heart.svg" alt="heart">
+      <a href="/">Saif Boukottaya</a>
+    </span>
+    <div class="rightNavbar">
+      <ul>
+        <li>
+          <a href="#">Projects</a>
+        </li>
+        <li>
+          <a href="#">CV</a>
+        </li>
+        <li>
+          <a href="#">About</a>
+        </li>
+        <li>
+          <a href="#">Contact</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss">
+@import '../scss/layout/navbar.scss';
+</style>
